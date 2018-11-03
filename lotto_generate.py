743b6c3415d73cd2bff3df0dcd41e8975ad0764b
Jeff Mcdowell <jeff.mcdowell@panerabread.com>
2018-11-03 16:17:50 -0500

    added additial disqualifiers

--- lotto_generate.py
+++ lotto_generate.py
@@ -5,10 +5,10 @@ import datetime
 num_dict = {}  # type: Dict[int, int]
 
 # Knobs to turn
-sample_size = 24    # how big of a set of numbers to choose from
+sample_size = 32    # how big of a set of numbers to choose from
 ticket_numbers = 8   # how many numbers to generate
-hot_number_weight = 3  # counter bonus for hot numbers
-random_generation = 5000  # how many random numbers to add to the formula
+hot_number_weight = 2  # counter bonus for hot numbers
+random_generation = 1000  # how many random numbers to add to the formula
 
 print "[[[ Missouri Lottery - Number Generator ]]]"
 print "Get the most recent csv from: http://www.molottery.com/winningNumbers.do?method=forward#Lotto"
@@ -31,6 +31,8 @@ with open('lo.csv') as csv_file:
                     number_total = num_dict[number]
                     num_dict[number] = number_total + 1
         line_count += 1
+
+print num_dict
 
 # Read the most recent lotto numbers (last year) as a subset and add weight
 with open('lo_2018.csv') as csv_file:
@@ -67,27 +69,48 @@ all_picks = []
 
 while len(all_picks) < ticket_numbers:
     ticket_nums = []
+    dq_nums = []
     rando = sorted(random.sample(range(1,len(lotto_set)), 6))
 
-    # Only a valid selection if there are >1 odd and >1 even numbers
-    odd_counter = 0
-    even_counter = 0
     for j in rando:
-        if j % 2 == 0:
-            even_counter += 1
-        else:
-            odd_counter += 1
         ticket_nums.append(lotto_set[j])
 
     ticket_nums.sort(key=int)
-    if (odd_counter >= 2) and (even_counter >= 2):
-        all_picks.append(ticket_nums)
-        print ticket_nums
 
-playing = raw_input("Playing these numbers? (Y or N)")
-if playing in ['y', 'yes', 'Y', "Yes", 'YES']:
-    with open("play_history.txt", "a") as logfile:
-        logfile.write((datetime.datetime.now().ctime()) + ":\n")
-        for num in all_picks:
-            logfile.write(str(num) + "\n")
-        logfile.write("\n")
+    # Analyze the number set
+    odd_counter = 0
+    even_counter = 0
+    high_numbers = 0
+    low_numbers = 0
+
+    for k in ticket_nums:
+        if k % 2 == 0:
+            even_counter += 1
+        else:
+            odd_counter += 1
+
+        if k < 22:
+            low_numbers += 1
+        else:
+            high_numbers += 1
+
+    if (odd_counter < 2) or (even_counter < 2):
+        dq_nums.append(str(ticket_nums) + "-DQ(odd/even)")
+        # print(str(ticket_nums) + "-DQ(odd/even)")
+    elif (low_numbers < 2) or (high_numbers < 2):
+        dq_nums.append(str(ticket_nums) + "-DQ(low/high)")
+        # print(str(ticket_nums) + "-DQ(low/high)")
+    else:
+        all_picks.append(ticket_nums)
+        # print str(ticket_nums) + " { o:" + str(odd_counter) + " e:" + str(even_counter) + " l:" + str(low_numbers) + " h:" + str(high_numbers) + "}"
+        print str(ticket_nums)
+
+# playing = raw_input("Playing these numbers? (Y or N)")
+# if playing in ['y', 'yes', 'Y', "Yes", 'YES']:
+#     with open("play_history.txt", "a") as logfile:
+#         logfile.write((datetime.datetime.now().ctime()) + ":\n")
+#         for num in all_picks:
+#             logfile.write(str(num) + "\n")
+#         for num in dq_nums:
+#             logfile.write(str(num) + "\n")
+#         logfile.write("\n")
